64b445ad3eaa9a52b007023ef449ca6144a39caa
Ramill <ram4ik@hotmail.com>
2024-01-06 18:50:21 +0200

    Initial Commit

---
+++ SharedButtonAnimation/SharedButtonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedButtonAnimationApp.swift
+//  SharedButtonAnimation
+//
+//  Created by admin on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SharedButtonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
