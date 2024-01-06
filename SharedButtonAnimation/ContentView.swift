1e5aa765d56653c904d3f11b21ad7fac27fed282
Ramill <ram4ik@hotmail.com>
2024-01-06 19:18:29 +0200

    init

--- SharedButtonAnimation/ContentView.swift
+++ SharedButtonAnimation/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var step1 = false
+    @State private var step2 = false
+    
+    var icons = ["paperplane", "swift", "bird"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                ForEach(icons.indices, id: \.self) { index in
+                    Image(systemName: icons[index])
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 50, height: 50)
+                        .padding()
+                        .scaleEffect(step2 ? 1 : 0)
+                        .animation(.spring(duration: 0.5).delay(Double(index) * 0.3) , value: step2)
+                }.padding()
+            }
+            .background(.secondary.opacity(0.4), in: RoundedRectangle(cornerRadius: 40))
+            .onAppear() {
+                step2.toggle()
+            }
+            Button("Reset") {
+                step1.toggle()
+                step2.toggle()
+            }.padding()
         }
-        .padding()
     }
 }
 
